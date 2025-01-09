cffcfebd8d6689456827bd01afe1b5e20d25b2cc
Odney Bleus <odneybleus@Odneys-MacBook-Air.local>
2025-01-09 14:00:06 -0500

    added variables to user model

--- src/main/java/com/BankApp/Models/User.java
+++ src/main/java/com/BankApp/Models/User.java
@@ -1,4 +1,37 @@
 package com.BankApp.Models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userID;
+
+    private String userName;
+
+    @Column(unique = true, nullable = false)
+    private String userEmail;
+
+
+    private String userPassword;
+    private String userAddress;
+    private String userPhoneNumber;
+    private LocalDate dateOfBirth;
+    private LocalDate createdDate;
+
+
+
 }
