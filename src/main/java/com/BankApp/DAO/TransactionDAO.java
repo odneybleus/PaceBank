81e9b1f5c22c1ee5ae17845ddcbb3c4bd18e26c9
Odney Bleus <odneybleus@Odneys-MacBook-Air.local>
2025-02-01 23:24:56 -0500

    added some transaction business logic

--- src/main/java/com/BankApp/DAO/TransactionDAO.java
+++ src/main/java/com/BankApp/DAO/TransactionDAO.java
@@ -3,5 +3,8 @@ package com.BankApp.DAO;
 import com.BankApp.Models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionDAO extends JpaRepository<Transaction, Integer> {
+
 }
--- src/main/java/com/BankApp/Services/TransactionService.java
+++ src/main/java/com/BankApp/Services/TransactionService.java
@@ -1,4 +1,35 @@
 package com.BankApp.Services;
 
+import com.BankApp.DAO.TransactionDAO;
+import com.BankApp.Models.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TransactionService {
+    private final TransactionDAO transactionDAO;
+
+    @Autowired
+    public TransactionService(TransactionDAO transactionDAO) {
+        this.transactionDAO = transactionDAO;
+    }
+
+    //TODO Get all transactions
+    public List<Transaction> getAllTransactions(){
+        return transactionDAO.findAll();
+    }
+
+    // TODO Retrieve all transactions for a specific account
+    public Optional<Transaction> getTransactionsByAccountId(int accountId) {
+        return transactionDAO.findById(accountId);
+    }
+
+    //TODO Retrieve transaction details by transaction ID
+   public Optional<Transaction> getTransactionById(int transactionId){
+        return transactionDAO.findById(transactionId);
+   };
+
 }
